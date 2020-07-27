0dd9bc0043c56d634cb2aee73ba0c57ccc8ba600
Reynier R <ralarcon9705@gmail.com>
2020-07-27 00:49:53 -0400

    Dynamic component with Ivy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'learning-apollo-client';
+
+  @ViewChild('container', { read: ViewContainerRef, static: false }) entry: ViewContainerRef;
+
+  constructor(private cfr: ComponentFactoryResolver) {}
+
+  ngAfterViewInit(): void {
+
+    // CREAR LA CLASE DEL COMPONENTE
+    @Component({
+      selector: 'custom-component',
+      template: '<h1>Hello Christ!!!</h1>'
+    }) class CustomComponent {}
+
+    const factory = this.cfr.resolveComponentFactory(CustomComponent);
+    this.entry.createComponent(factory);
+  }
 }
